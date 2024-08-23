7983eb030319f9b4ccf39e25eccc594f04369133
duduBmoon21 <eleanortefera21@gmail.com>
2024-08-23 02:11:34 -0700

    d

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import "./bootstrap";
 import $ from 'jquery';
 window.jQuery = window.$ = $
-// import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import { Livewire, Alpine } from '@livewire';
 
 import sort from '@alpinejs/sort'
